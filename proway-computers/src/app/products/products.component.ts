fbdc81363b7225ec0566dcbd75271a4b13cc1727
Dreyyyyy <andreydias@alunos.utfpr.edu.br>
2023-03-16 16:30:25 -0300

    search bar finished

--- proway-computers/src/app/products/products.component.ts
+++ proway-computers/src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { IProduto, produtos } from '../products';
 import { ProductsService } from '../products.service';
 
@@ -10,11 +11,22 @@ import { ProductsService } from '../products.service';
 export class ProductsComponent implements OnInit{
   products: IProduto[] | undefined;
 
-  constructor(private productService: ProductsService) {
+  constructor(private productService: ProductsService, private route:ActivatedRoute) {
 
   }
   
   ngOnInit(): void {
-    this.products = this.productService.getAll();
+    const products = this.productService.getAll();
+
+    this.route.queryParamMap.subscribe(params => {
+      const description = params.get("descricao")?.toLowerCase();
+
+      if (description) {
+        this.products = products.filter(product => product.descricao.toLowerCase().includes(description));
+        return;
+      }
+
+      this.products = products;
+    });
   }
 }
