abb2d3c03866b1291a1f0870a5e243cd3cc95ca7
Dreyyyyy <andreydias@alunos.utfpr.edu.br>
2023-04-01 15:20:13 -0300

    Title case exercise completed

---
+++ angular-course-code-with-mosh/title-case-exercise/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { TitleCasePipe } from './title-case.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TitleCasePipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
